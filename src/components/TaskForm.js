9571a7c0079e4cd08c0414bc02ee15a6c0e960e5
Muki Dev <mukidev881@gmail.com>
2025-04-15 12:22:14 +0000

    taskForm file update

---
+++ src/components/TaskForm.js
@@ -0,0 +1,40 @@
+import React, { useState } from 'react';
+
+function TaskForm({addTask}) {
+	const[text,setText] = useState('');
+	const [category,setCategory] = useState('personnel');
+
+		const handleSubmit = (e) => {
+			e.preventDefault();
+			if(!text) return;
+			const newTask = {
+				id: Date.naw(),
+				text,
+				category,
+				completed: false,
+			};
+
+			addTask(newTask);
+			setText('')
+			setCategory('personne1')
+		};
+
+		return(
+				<form className='task-form' onSubmit={handleSubmit}>
+					<input
+						type="text"
+						placeholder="New Task"
+						value={text}
+						onChange={(e) => setText(e.target.value)}
+					 />
+					 <select value={category} onChange={(e) => setCategory(e.target.value)}>
+					 	<option value='personnel'>Personnel</option>
+					 	<option value='important'>Important</option>
+					 	<option value='travail'>Travail</option>
+					 </select>
+					 <button type='submit'>Ajouter</button>
+				</form>
+			);
+}
+
+export default TaskForm;
